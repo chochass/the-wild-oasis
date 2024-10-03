525fd8906a7c0b5334200b0832e241c10c6c77ef
Chavdar Dimov <chavdar.dimovv@gmail.com>
2024-10-03 21:54:29 +0300

    Refactor code to use React Router for routing and add UI components

--- src/ui/Heading.jsx
+++ src/ui/Heading.jsx
@@ -7,19 +7,22 @@ const Heading = styled.h1`
       font-size: 3rem;
       font-weight: 600;
     `}
+
   ${(props) =>
     props.as === "h2" &&
     css`
       font-size: 2rem;
       font-weight: 600;
-    `} 
+    `}
+    
     ${(props) =>
     props.as === "h3" &&
     css`
       font-size: 2rem;
       font-weight: 500;
     `}
-  line-height:1.4;
+    
+  line-height: 1.4;
 `;
 
 export default Heading;
