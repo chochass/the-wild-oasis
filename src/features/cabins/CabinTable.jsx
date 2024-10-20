40987610cf780035c6189a21071b404906776281
Chavdar Dimov <chavdar.dimovv@gmail.com>
2024-10-20 16:42:39 +0300

    Refactor CabinTable to filter and sort cabins based on search parameters

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -1,5 +1,5 @@
+import { useSearchParams } from "react-router-dom";
 import { useCabins } from "./useCabins";
-import styled from "styled-components";
 import Spinner from "../../ui/Spinner";
 import CabinRow from "./CabinRow";
 import Table from "../../ui/Table";
@@ -7,7 +7,34 @@ import Menus from "../../ui/Menus";
 
 function CabinTable() {
   const { isLoading, cabins } = useCabins();
+
+  const [searchParams] = useSearchParams();
+
   if (isLoading) return <Spinner />;
+
+  // 1. FILTER
+  const filterValue = searchParams.get("discount") || "all";
+
+  let filteredCabins;
+  if (filterValue === "all") {
+    filteredCabins = cabins;
+  }
+  if (filterValue === "no-discount") {
+    filteredCabins = cabins.filter((cabin) => cabin.discount === 0);
+  }
+  if (filterValue === "with-discount") {
+    filteredCabins = cabins.filter((cabin) => cabin.discount > 0);
+  }
+
+  // 2) SORT
+  const sortBy = searchParams.get("sortBy") || "startDate-asc";
+  const [field, direction] = sortBy.split("-");
+  const modifier = direction === "asc" ? 1 : -1;
+  const sortedCabins = filteredCabins?.sort((a, b) =>
+    typeof a[field] === "string"
+      ? a[field].localeCompare(b[field]) * modifier
+      : (a[field] - b[field]) * modifier
+  );
 
   return (
     <Menus>
@@ -21,7 +48,7 @@ function CabinTable() {
           <div></div>
         </Table.Header>
         <Table.Body
-          data={cabins}
+          data={sortedCabins}
           render={(cabin) => <CabinRow cabin={cabin} key={cabin.id} />}
         />
       </Table>
