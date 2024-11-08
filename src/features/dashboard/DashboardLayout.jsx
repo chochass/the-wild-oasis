bfe79dc4f5db563a06b6cf81dd969ca4e6250858
Chavdar Dimov <chavdar.dimovv@gmail.com>
2024-11-08 22:07:38 +0200

    Implement TodayActivity component with loading state and activity display; add CheckoutButton for check-out functionality

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 import Stats from "./Stats";
 import Spinner from "../../ui/Spinner";
+import TodayActivity from "../check-in-out/TodayActivity";
 import { useRecentBookings } from "./useRecentBookings";
 import { useRecentStays } from "./useRecentStays";
 import { useCabins } from "../cabins/useCabins";
@@ -29,7 +30,7 @@ function DashboardLayout() {
         numDays={numDays}
         cabinCount={cabins.length}
       />
-      <div>Today's activity</div>
+      <TodayActivity />
       <DurationChart confirmedStays={confirmedStays} />
       <SalesChart bookings={bookings} numDays={numDays} />
     </StyledDashboardLayout>
